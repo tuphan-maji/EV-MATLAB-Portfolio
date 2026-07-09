138159535cc7a71a6e3f70eb8591aa11bdbc0579
Tuphan Maji <tuphanmail.com@gmail.com>
2026-07-10 02:28:20 +0530

    Add files via upload

---
+++ 06_Embedded_C/02_CAN_Message_Parser/CAN_Massage_Parser.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+
+#include <stdint.h>
+#include<stdio.h>
+int main() {
+   uint8_t temperature = 45;
+   uint16_t voltage=3500;
+   printf("Temperature = %u C ", temperature);
+   printf("Voltage = %u ", voltage);
+return 0;
+}
